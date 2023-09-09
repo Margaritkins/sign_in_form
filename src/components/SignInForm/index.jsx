b079f1f159c714036e057426aa7108c8eac30298
Margarita <te120883ama@gmail.com>
2023-09-09 22:03:28 +0200

    Add input login, check, radio

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import cx from "classnames";
 import styles from "./SignInForm.module.css";
 const initialValue = {
+  male: false,
+  female: false,
+  check: false,
+  login: "",
   email: "",
   password: "",
+  maleValid: true,
+  femaleValid: true,
+  checkValid: true,
+  loginValid: true,
   emailValid: true,
   passwordValid: true,
 };
@@ -30,6 +39,15 @@ class SignInForm extends Component {
   //   handlePassword=(e)=>{
   //     this.setState ({password:e.target.value})
   //   }
+  hndleRadio = ({ target: { name, checked } }) => {
+    // console.dir(target);
+    this.setState({ [name]: checked });
+  };
+
+  hndleCheck = ({ target: { name, checked } }) => {
+    // console.dir(target);
+    this.setState({ [name]: checked });
+  };
 
   hndleInput = ({ target: { name, value } }) => {
     this.setState({
@@ -39,19 +57,82 @@ class SignInForm extends Component {
   };
 
   render() {
-    const { email, password, emailValid, passwordValid } = this.state;
-    const classesEmail = `${styles.input} ${
-      emailValid ? null : styles.invalid
-    }`;
-    const classesPassword = `${styles.input} ${
-      passwordValid ? null : styles.invalid
-    }`;
-    // const classesPassword = [styles.input];
-    // if(passwordValid === false){
-    //   classesPassword.push(styles.invalid)
-    // }
+    const {
+      male,
+      female,
+      check,
+      login,
+      email,
+      password,
+      maleValid,
+      femaleValid,
+      checkValid,
+      loginValid,
+      emailValid,
+      passwordValid,
+    } = this.state;
+    const classMale = cx(styles.imput, {
+      [styles.invalid]: !maleValid,
+    });
+    const classFemale = cx(styles.imput, {
+      [styles.invalid]: !femaleValid,
+    });
+    const classesCheck = cx(styles.input, {
+      [styles.invalid]: !checkValid,
+    });
+    const classesLogin = cx(styles.input, {
+      [styles.invalid]: !loginValid,
+    });
+    const classesEmail = cx(styles.input, {
+      [styles.invalid]: !emailValid,
+    });
+    const classesPassword = cx(styles.input, {
+      [styles.invalid]: !passwordValid,
+    });
     return (
       <form className={styles.form} onSubmit={this.handleForm}>
+        <label htmlFor="male">
+          <input
+            className={classMale}
+            value={male}
+            onChange={this.hndleRadio}
+            id="male"
+            name="radio"
+            type="radio"
+          />
+          male
+        </label>
+        <label htmlFor="female">
+          <input
+            className={classFemale}
+            value={female}
+            onChange={this.hndleRadio}
+            id="female"
+            name="radio"
+            type="radio"
+          />
+          female
+        </label>
+
+        <label htmlFor="">
+          <input
+            className={classesCheck}
+            checked={check}
+            onChange={this.hndleCheck}
+            name="check"
+            type="checkbox"
+          />
+          check me
+        </label>
+
+        <input
+          className={classesLogin}
+          value={login}
+          onChange={this.hndleInput}
+          type="text"
+          name="login"
+          placeholder="your login"
+        />
         <input
           className={classesEmail}
           value={email}
